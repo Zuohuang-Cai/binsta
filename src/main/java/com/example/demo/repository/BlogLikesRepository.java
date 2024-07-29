3a737e79f5d56bd67bb7e8940cd8ef6a3051d8c1
Zuohuang Cai <zc26226@gmail.com>
2024-07-29 15:30:22 +0200

    images for seeder

--- src/main/java/com/example/demo/repository/BlogLikesRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.BlogLikes;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface BlogLikesRepository extends JpaRepository<BlogLikes, Long> {
-}
--- src/main/java/com/example/demo/repository/BlogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Blog;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BlogRepository extends CrudRepository<Blog, Long> {
-}
